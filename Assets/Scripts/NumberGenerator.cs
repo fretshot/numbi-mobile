7cd5f57de76932705c94b16aa1b2bb0f42ec86b7
Oscar Alemán <fretshot@gmail.com>
2019-07-06 14:19:58 -0500

    first commit

---
+++ Assets/Scripts/NumberGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberGenerator : MonoBehaviour{
+
+    public GameObject[] numbers;
+
+    public float minTime = 2f;
+    public float maxTime = 6f;
+
+    void Start(){
+
+        Invoke("instantiateNumber", Random.Range(minTime, maxTime));
+
+    }
+
+    void instantiateNumber() {
+
+        int numero = Random.Range(0, numbers.Length);
+
+        Instantiate(numbers[numero], transform.position, Quaternion.identity);
+
+        Invoke("instantiateNumber", Random.Range(minTime, maxTime));
+    }
+
+
+}
---
+++ Assets/Scripts/NumberMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberMovement : MonoBehaviour{
+
+    public float speed = 5f;
+
+    void Update() {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+}
